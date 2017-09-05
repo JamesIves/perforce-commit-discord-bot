158eff03bcc8e86219083dc855da5a43767c397c
James Ives <iam@jamesiv.es>
2017-09-04 20:24:41 -0500

    Init :rocket:
    Starting up service.

---
+++ app.py
@@ -0,0 +1,49 @@
+import os
+import subprocess
+import discord
+import time
+
+from settings import *
+
+client = discord.Client()
+
+global_store = {
+  'latest_change': ''
+}
+
+def check_for_changes():
+  p4_changes = subprocess.Popen('p4 changes -t -m 1', stdout=subprocess.PIPE, shell=True)
+  output = p4_changes.stdout.read().decode("utf-8")
+  print('checking')
+  print(output)
+  if output != global_store['latest_change']:
+    #lets print the chnage
+    print('they are not the same')
+    global_store['latest_change'] = output
+
+    return output
+
+  else: 
+    print('they match, disregard')
+    return ''
+
+@client.event
+async def post_changes():
+  print('posting changes')
+  payload = check_for_changes()
+
+  if payload != '':
+    await client.send_message(discord.Object(id='276383733945204736'), payload)
+
+  else:
+    return
+
+@client.event
+async def on_ready():
+  t=time.time()
+  while True:
+    if time.time()-t>30:
+        await post_changes()
+        t=time.time()
+
+client.run(DISCORD_BOT_TOKEN)
